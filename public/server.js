cc66ef5ce4444b2c1dc0a9bbb5a80e5b4cee88cd
A Hoerman <awhoerman@me.com>
2020-03-15 19:50:31 -0400

    add notes to db.json when saved

--- public/server.js
+++ public/server.js
@@ -1,6 +1,7 @@
 //Dependecies
 var express = require("express");
 var path = require("path");
+let fs = require("fs");
 
 //Set up Express App
 var app = express();
@@ -22,3 +23,20 @@ app.get("/", function(req, res) {
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
 });
+
+app.get("/api/notes", function(req, res) {
+    fs.readFile('db.json', (err, data) => {
+        if (err) throw err;
+        return data;
+      });;
+});
+
+app.post("/api/notes", function(req, res) {
+    let newNote = req.body;
+    console.log(newNote);
+    fs.appendFile("db.json", JSON.stringify(newNote), (err) => {
+        if (err) throw err;
+        console.log('The note was appended to file!');
+      });
+  });
+
